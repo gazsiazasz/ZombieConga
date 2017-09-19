4bd1a51306ae615618d0e36931547acb20c27b7b
Gajdos Gábor <gazsiazasz@gmail.com>
2017-09-19 07:41:04 +0200

    Initial Commit

---
+++ ZombieConga/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ZombieConga
+//
+//  Created by Gajdos Gábor on 2017. 09. 19..
+//  Copyright © 2017. Gajdos Gábor. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ ZombieConga/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ZombieConga
+//
+//  Created by Gajdos Gábor on 2017. 09. 19..
+//  Copyright © 2017. Gajdos Gábor. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
